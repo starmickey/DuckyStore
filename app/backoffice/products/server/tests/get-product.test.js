e1033580e551fb1430e968c1f6861083b60a75ef
unknown <micaelaestrellamorales@gmail.com>
2023-08-01 09:38:55 -0300

    Update search server test

--- app/backoffice/products/server/tests/get-product.test.js
+++ app/backoffice/products/server/tests/get-product.test.js
@@ -12,18 +12,23 @@ beforeEach(async () => {
 describe("GET /products/search", () => {
   it("find product by introducing its name", async () => {
     const res = await request(app).get("/products/search?q=carrot");
+    result = res._body.response.find( element => element.name = 'carrot');
     expect(res.statusCode).toBe(200);
-    expect(res._body.response.name).toBe('carrot');
-    expect(res._body.response.description).toBeDefined();
-    // expect(res._body.response.status).toBe('ACTIVE');
-    expect(res._body.response.price).toBeDefined();
+    expect(res._body.response).toBeDefined();
+    expect(result.name).toBe('carrot');
+    expect(result.status).toBe('ACTIVE');
+    expect(result.price).toBeDefined();
 
   });
 
   it("case independent search", async () => {
     const res = await request(app).get("/products/search?q=caRRot");
+    result = res._body.response.find( element => element.name = 'carrot');
     expect(res.statusCode).toBe(200);
-    expect(res._body.response.name).toBe('carrot');
+    expect(res._body.response).toBeDefined();
+    expect(result.name).toBe('carrot');
+    expect(result.status).toBe('ACTIVE');
+    expect(result.price).toBeDefined();
 
   });
 });
